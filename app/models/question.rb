629b6fa63cbd992b26ce8be1e5379ac449b83616
Uday <uday88k@gmail.com>
2013-05-24 21:49:02 -0600

    still changing the linkup of tables in views for dropdown

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,8 +5,8 @@ class Question < ActiveRecord::Base
   accepts_nested_attributes_for :answers, allow_destroy: true
 
   def categories
-  	#[['Section 12', 'Section 12'],['Section 2', 'Section 2']].freeze
-  	{'section 1' => 'Section 1', 'section 2' => 'Section 2' }.freeze
+  	#{'section 1' => 'Section 1', 'section 2' => 'Section 2' }.freeze
+  	{'Hello' => 'hello', 'World' => 'world'}
   end
 
 end
