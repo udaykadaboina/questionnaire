f94d2e44309c327f0b4d1f0ac9243bfcb2ae20f6
Uday <uday88k@gmail.com>
2013-05-30 18:21:43 -0600

    made some changes(forgotten)

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,7 +3,7 @@ class Survey < ActiveRecord::Base
   has_many :questions
   accepts_nested_attributes_for :questions, allow_destroy: true 
 
-  def surveys
-  	{'APR' => 'APR'}.freeze
+  def self.survey_sections
+  	{'Section 1' => 'Section 1', 'Section 2' => 'Section 2'}.freeze
   end
 end
